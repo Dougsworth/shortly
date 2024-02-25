eb4552ed5c09debb6ad1562f55f9b11f89b352ac
Admin 47 <Admin 47@DESKTOP-5K68MR1>
2024-02-25 14:46:54 -0500

    Add project files.

---
+++ shortly/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace shortly.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
